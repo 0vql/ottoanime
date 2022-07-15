a14528888be6bec3c76d198c830884d9752d0a70
otto <otmankhess@gmail.com>
2022-07-15 01:54:23 +0100

    all

--- fastapi/main.py
+++ fastapi/main.py
@@ -17,31 +17,31 @@ app.add_middleware(
 )
 
 
-@app.get('/recently/{page}')
+@app.get('/api/recently/{page}')
 def recently(page: int):
     recently = GogoanimeParser.get_recently_uploaded(page=page)
     return json.loads(recently)
 
 
-@app.get('/popular/{page}')
+@app.get('/api/popular/{page}')
 def popular(page: int):
     popular = GogoanimeParser.popular(page=page)
     return json.loads(popular)
 
 
-@app.get('/genre/{genre}/{page}')
+@app.get('/api/genre/{genre}/{page}')
 def genre(genre: str, page: int):
     genre = GogoanimeParser.genre(genre_name=genre, page=page)
     return genre
 
 
-@app.get('/details/{animeid}')
+@app.get('/api/details/{animeid}')
 def details(animeid: str):
     detail = GogoanimeParser.details(animeid=animeid)
     return detail
 
 
-@app.get('/{id}/{episode_num}')
+@app.get('/api/{id}/{episode_num}')
 def episode(id: str, episode_num: int):
     episode = GogoanimeParser.episode(animeid=id, episode_num=episode_num)
     return episode
--- utils/URLS.js
+++ utils/URLS.js
@@ -1,9 +1,9 @@
 export const URL = {
-  POPULAR: "http://localhost:8000/popular/",
-  RECENT: "http://localhost:8000/recently/",
-  DETAILS: "http://localhost:8000/details/",
-  EPLINK: "http://localhost:8000/",
-  GENRES: "http://localhost:8000/genre/",
-  SEARCH: "http://localhost:8000/search/",
-  SEASON: "http://localhost:8000/new-season/",
+  POPULAR: "http://localhost:8000/api/popular/",
+  RECENT: "http://localhost:8000/api/recently/",
+  DETAILS: "http://localhost:8000/api/details/",
+  EPLINK: "http://localhost:8000/api/",
+  GENRES: "http://localhost:8000/api/genre/",
+  SEARCH: "http://localhost:8000/api/search/",
+  SEASON: "http://localhost:8000/api/new-season/",
 };
