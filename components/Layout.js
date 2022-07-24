d0227b21c3e608beec8c373183926c08431a640c
otto <otmankhess@gmail.com>
2022-07-24 16:59:46 +0100

    sssr

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 
-const Layout = ({ children, title = "Animex" }) => {
+const Layout = ({ children, title = "Animex Stream" }) => {
   return (
     <div className="w-full justify-center items-center min-h-screen mx-auto lg:h-full lg:w-10/12 mt-20">
       <Head>
         <title>{title}</title>
+        <meta property="og:title" content="My page title" key="title" />
+
+        <meta property="og:description" content="waaaawlad lqhaab gigigig" />
       </Head>
       {children}
     </div>
--- components/card/Card.js
+++ components/card/Card.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 import { useSelector } from "react-redux";
 import Link from "next/link";
 import { AiFillCalendar } from "react-icons/ai";
+import Loading from "../Loading";
+import { useState } from "react";
+import LazyLoad from 'react-lazyload';
+
 
 const MovieWrapper = styled.a`
   display: flex;
@@ -60,6 +64,18 @@ const MovieImg = styled.img`
   }
 `;
 
+const ImgLoading = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  min-height: 300px;
+  border-radius: 0.8rem;
+  box-shadow: 0rem 2rem 5rem var(--shadow-color);
+  transition: all 100ms cubic-bezier(0.645, 0.045, 0.355, 1);
+`;
+
 const Title = styled.span`
   width: 100%;
   overflow: hidden;
@@ -81,8 +97,11 @@ const DetailsWrapper = styled.div`
 `;
 
 const Card = ({ title, id, url, heading, image_url, episode, released }) => {
+  const [loaded, setLoaded] = useState(false);
+
   const theme = useSelector((state) => state.theme);
   return (
+    <LazyLoad height={200} offset={200}>
     <Link
       href={
         episode
@@ -92,15 +111,20 @@ const Card = ({ title, id, url, heading, image_url, episode, released }) => {
           : `/details/${url}`
       }
     >
-      <a>
         <MovieWrapper
           className={`relative ${theme.card.text} ${theme.card.bghover} cursor-pointer items-center rounded-xl w-full text-center justify-start flex flex-col  `}
           card={theme.card}
         >
+          {!loaded ? (
+          <ImgLoading>
+            <Loading />
+          </ImgLoading>
+        ) : null}
           <MovieImg
             className="w-full object-cover rounded-xl h-[11rem]  xl:h-70 md:h-72 lg:h-66"
+            onLoad={() => setLoaded(true)}
+
             src={image_url}
-            loading="lazy"
             alt={title}
           />
 
@@ -125,8 +149,9 @@ const Card = ({ title, id, url, heading, image_url, episode, released }) => {
             )}
           </DetailsWrapper>
         </MovieWrapper>
-      </a>
+      
     </Link>
+    </LazyLoad>
   );
 };
 
