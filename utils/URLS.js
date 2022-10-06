08ceb7e3f6686a851c06281fdaf17848efdcef2a
otto <otmankhess@gmail.com>
2022-10-06 17:09:46 +0100

    'railway'

--- utils/URLS.js
+++ utils/URLS.js
@@ -1,7 +1,7 @@
 export const URL = {
   POPULAR: "https://ottogo.vercel.app/api/popular/",
   RECENT: "https://ottogo.vercel.app/api/latest/",
-  DETAILS: "https://ottogo.vercel.app/api/details/",
+  DETAILS: "https://web-production-892e.up.railway.app/api/details/",
   EPLINK: "https://ottogo.vercel.app/api/",
   GENRES: "https://ottogo.vercel.app/api/category/",
   SEARCH: "https://ottogo.vercel.app/api/search/",
